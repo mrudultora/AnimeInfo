82ed6dc79e2c67ae70f5fc5856de18d8c939a19c
mrudultora <mrudultora@gmail.com>
2026-02-24 03:25:53 +0530

    Minor Changes

--- app/src/main/java/com/mrudultora/animeinfo/ui/theme/Theme.kt
+++ app/src/main/java/com/mrudultora/animeinfo/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun SeekhoAssignmentTheme(
+fun AnimeInfoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
