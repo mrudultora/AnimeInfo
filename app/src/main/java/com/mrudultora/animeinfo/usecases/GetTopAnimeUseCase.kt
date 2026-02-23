76d89b35aa4a0506d59672a0f70999104ed4b041
mrudultora <mrudultora@gmail.com>
2026-02-24 04:41:54 +0530

    Added GetTopAnimeUseCase

---
+++ app/src/main/java/com/mrudultora/animeinfo/usecases/GetTopAnimeUseCase.kt
@@ -0,0 +1,15 @@
+package com.mrudultora.animeinfo.usecases
+
+import androidx.paging.PagingData
+import com.mrudultora.animeinfo.network.AnimeInfoRepository
+import com.mrudultora.animeinfo.room.models.AnimeInfoEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetTopAnimeUseCase @Inject constructor(
+    private val repository: AnimeInfoRepository
+) {
+    operator fun invoke(): Flow<PagingData<AnimeInfoEntity>> {
+        return repository.getTopAnime()
+    }
+}
