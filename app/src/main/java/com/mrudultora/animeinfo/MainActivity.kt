e68c07dd7ebe9323188df20c136a7ecf53ef7623
mrudultora <mrudultora@gmail.com>
2026-02-24 06:08:32 +0530

    Updated mainactivity and added support for anime nav graph

--- app/src/main/java/com/mrudultora/animeinfo/MainActivity.kt
+++ app/src/main/java/com/mrudultora/animeinfo/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.mrudultora.animeinfo.navigation.AnimeNavGraph
 import com.mrudultora.animeinfo.ui.theme.AnimeInfoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,28 +23,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             AnimeInfoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Seekho Assignment",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .consumeWindowInsets(innerPadding)
+                    ) {
+                        AnimeNavGraph(navController = rememberNavController())
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimeInfoTheme {
-        Greeting("Android")
     }
 }
