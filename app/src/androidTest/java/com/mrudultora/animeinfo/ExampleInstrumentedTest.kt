e336b3093b06f916febcc6cfad640f4507439250
mrudultora <mrudultora@gmail.com>
2026-02-25 04:03:00 +0530

    Updated project name and theme name

--- app/src/androidTest/java/com/mrudultora/animeinfo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mrudultora/animeinfo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mrudultora.seekhoassignment", appContext.packageName)
+        assertEquals("com.mrudultora.animeinfo", appContext.packageName)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Seekho-Assignment"
+rootProject.name = "AnimeInfo"
 include(":app")
